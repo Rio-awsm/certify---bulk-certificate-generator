e9092f75a11dd81fe12695d2de9cbed15767fb70
Supriyo Saha <mm.saha1982@gmail.com>
2024-11-30 03:53:22 +0530

    basic folder structure

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "bg1":"url('/src/assets/bg.jpg')",
+        "bg2":"url('/src/assets/udemybg.png')",
+      }
+    },
+  },
+  plugins: [require("daisyui")],
+}
